365106bd61ca19a331524780478ac768527fcddf
TTVDannyg82 <63794287+TTVDannyg82@users.noreply.github.com>
2020-07-05 20:43:56 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,110 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("The server is ready to be OOFED");
+});
+ 
+let prefix = "o!";
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
+
+if(message.content.startsWith("oof"){
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+message.channel.send("OOF")
+ 
+client.login("SuperSecretBotTokenHere");
